3aab0ac77354037ed7dc70a9127fca5327ce9633
JJShen2000 <40858520+JJShen2000@users.noreply.github.com>
2021-06-05 18:36:32 +0800

    delete before update

--- Data/auto_data2db.py
+++ Data/auto_data2db.py
@@ -3,17 +3,15 @@ import subprocess
 
 if __name__ == '__main__':
     tables = ['daily_history_price', 'fruit', 'fruit_location', 'fruit_month', 'location', 'market', 'monthly_history_price']
-    '''con = sqlite3.connect('database.sqlite')
+    con = sqlite3.connect('database.sqlite')
     cur = con.cursor()
-    cur.execute('.mode csv')
 
-    for t in tables:
-        cmd = ".import "+t+".csv "+t+"s"
-        cur.execute(cmd)'''
     cmds = []
     for t in tables:
+        cur.execute('delete from '+t+"s;")
         cmds.append(".import "+t+".csv "+t+"s")
-        print(".import "+t+".csv "+t+"s")
+        #print(".import "+t+".csv "+t+"s")
+    con.commit()
     subprocess.call(["sqlite3", "database.sqlite", "-cmd",".mode csv",cmds[0], cmds[1], cmds[2], cmds[3],cmds[4], cmds[5], cmds[6]])
     '''subprocess.call(".mode csv")'''
     '''for t in tables:
